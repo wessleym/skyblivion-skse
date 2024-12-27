dc29370448ec9a485954515afb2cced7a52971fb
wessleym <wessleym@hotmail.com>
2024-12-27 09:47:09 -0600

    Boost changes

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@
 #include <stdint.h>
 #include <windows.h>
 #include "SKSE/Logger.h"
+
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/fstream.hpp>
  
 /* __declspec(dllimport) */double g_silent_voice_duration_seconds;
 /* __declspec(dllimport) */int g_is_obscript_say_say_to;
@@ -477,6 +481,36 @@ namespace RE {
 			}
 		}
 
+		//https://github.com/eeveelo/PapyrusUtil/blob/6212a5cdedbaceb5d805501b9518921ce5423e76/MiscUtil.cpp#L512
+		std::vector<BSFixedString> FilesInFolder(StaticFunctionTag* base, BSFixedString dirpath, BSFixedString extension) {
+            std::vector<BSFixedString> arr;
+            if (dirpath.data && dirpath.data[0] != '\0') {
+                fs::path dir(dirpath.data);
+                fs::directory_iterator end_iter;
+                if (fs::exists(dir) && fs::is_directory(dir)) {
+                    std::string ext;
+                    if (extension.data[0] == '.')
+                        ext = extension.data;
+                    else {
+                        ext = ".";
+                        ext.append(extension.data);
+                    }
+                    _MESSAGE("dir: %s ext: %s", dirpath.data, ext.c_str());
+                    for (fs::directory_iterator dir_iter(dir); dir_iter != end_iter; ++dir_iter) {
+                        if (fs::is_regular_file(dir_iter->status())) {
+                            fs::path filepath = dir_iter->path();
+                            std::string file = filepath.filename().generic_string();
+                            // std::string ext = filepath.extension().generic_string();
+                            //_MESSAGE("file: %s ext: %s", file.c_str(), ext.c_str());
+                            if (ext == ".*" || boost::iequals(filepath.extension().generic_string(), ext))
+                                arr.push_back(BSFixedString(file.c_str()));
+                        }
+                    }
+                }
+            }
+            return arr;
+        }
+
 		bool RegisterFuncs(BSScript::Internal::VirtualMachine* a_vm) {
 
 			_MESSAGE("Initializing ObScript hooks...");
@@ -597,14 +631,6 @@ SKSEPluginLoad(const SKSE::LoadInterface* skse) {
         _FATALERROR("Loaded in editor. Marking as incompatible.");
         return false;
     }
-
-    /*switch (a_skse->RuntimeVersion()) {
-    case RUNTIME_VERSION_1_5_97:
-        break;
-    default:
-        _FATALERROR("Unsupported runtime version %08X!\n", a_skse->RuntimeVersion());
-        return false;
-    }*/
 
     auto papyrus = SKSE::GetPapyrusInterface();
     if (!papyrus->Register(RE::ObScriptHooks::RegisterFuncs)) {
